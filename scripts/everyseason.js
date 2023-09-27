b648936c802b31b7db951a02a9dcd40b9b5c425b
Matt Cappleman <matt.cappleman@gmail.com>
2023-09-27 10:11:41 -0400

    all the seasons

--- scripts/everyseason.js
+++ scripts/everyseason.js
@@ -1,60 +1,61 @@
 $(document).ready(function () {
+  var table = $("#everySeasonTable").DataTable({
+    order: [[0, "desc"]],
+    lengthMenu: [
+      [12, 36, -1],
+      ["12", "36", "All"],
+    ],
+    data: everySeason.seasons,
+    columns: everySeason.headers,
+  });
 
-    var table = $('#everySeasonTable').DataTable({
-        order: [[0, "desc"]],
-        lengthMenu: [[12, 36, -1], ['12', '36', 'All']],
-        data: everySeason.seasons,
-        columns: everySeason.headers
+  $("#everySeasonTable tfoot th").each(function (i) {
+    var title = $(this).text();
+    var htmlString =
+      '<input type="text" class="form-control column-search" placeholder="' +
+      everySeason.headers[i].title +
+      '" />';
+    $(this).html(htmlString);
+  });
+
+  // Apply the search
+  table.columns().every(function () {
+    var that = this;
+
+    $("input", this.footer()).on("keyup change", function () {
+      if (that.search() !== this.value) {
+        that.search(this.value).draw();
+      }
     });
+  });
 
-    $('#everySeasonTable tfoot th').each(function (i) {
-
-        var title = $(this).text();
-        var htmlString = '<input type="text" class="form-control column-search" placeholder="' + everySeason.headers[i].title + '" />';
-        $(this).html(htmlString);
-
-    });
-
-    // Apply the search
-    table.columns().every(function () {
-        var that = this;
-
-        $('input', this.footer()).on('keyup change', function () {
-            if (that.search() !== this.value) {
-                that
-                    .search(this.value)
-                    .draw();
-            }
-        });
-    });
-
-    $('#everySeasonTable_filter').addClass('hidden');
-
+  $("#everySeasonTable_filter").addClass("hidden");
 });
 
 var everySeason = {
-    "headers": [
-        { title: "Year" },
-        { title: "Name" },
-        { title: "Games" },
-        { title: "Wins" },
-        { title: "Losses" },
-        { title: "Ties" },
-        { title: "Points For" },
-        { title: "Points Against" },
-        { title: "Points per Game" },
-        { title: "PA per game" },
-        { title: "Total Wins" },
-        { title: "Total Losses" },
-        { title: "Total Ties" },
-        { title: "Expected Win Percentage" },
-        { title: "Expected Wins" },
-        { title: "Expected Losses" },
-        { title: "Excess Wins" }
-    ],
-    "seasons": []
-}
+  headers: [
+    { title: "Year" },
+    { title: "Name" },
+    { title: "Games" },
+    { title: "Wins" },
+    { title: "Losses" },
+    { title: "Ties" },
+    { title: "Points For" },
+    { title: "Points Against" },
+    { title: "Points per Game" },
+    { title: "PA per game" },
+    { title: "Total Wins" },
+    { title: "Total Losses" },
+    { title: "Total Ties" },
+    { title: "Expected Win Percentage" },
+    { title: "Expected Wins" },
+    { title: "Expected Losses" },
+    { title: "Excess Wins" },
+  ],
+  seasons: [],
+};
 
+everySeason.seasons = combineArrays(everySeason.seasons, data2023, 2023);
 everySeason.seasons = combineArrays(everySeason.seasons, data2022, 2022);
 everySeason.seasons = combineArrays(everySeason.seasons, data2021, 2021);
 everySeason.seasons = combineArrays(everySeason.seasons, data2020, 2020);
